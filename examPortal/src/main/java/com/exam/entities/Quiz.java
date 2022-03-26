f9dd57cc65f97e020e0008e4b4bea1b87eb17468
Rohit kumar <78663043+rohit-kumar-kushwaha@users.noreply.github.com>
2022-03-26 20:39:26 +0530

    exam related table coding

---
+++ examPortal/src/main/java/com/exam/entities/Quiz.java
@@ -0,0 +1,113 @@
+package com.exam.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "quiz")
+public class Quiz {
+	
+	@Id
+	private long qid;
+	private String title;
+	private String description;
+	private String maxMarks;
+	private String numberOfQuestions;
+	private boolean active = false;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Category category;
+	
+	
+	@OneToMany(mappedBy = "quiz", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JsonIgnore
+	private Set<Question> questions = new HashSet<>();
+	
+	
+	public Quiz() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public long getQid() {
+		return qid;
+	}
+
+
+	public void setQid(long qid) {
+		this.qid = qid;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getMaxMarks() {
+		return maxMarks;
+	}
+
+
+	public void setMaxMarks(String maxMarks) {
+		this.maxMarks = maxMarks;
+	}
+
+
+	public String getNumberOfQuestions() {
+		return numberOfQuestions;
+	}
+
+
+	public void setNumberOfQuestions(String numberOfQuestions) {
+		this.numberOfQuestions = numberOfQuestions;
+	}
+
+
+	public boolean isActive() {
+		return active;
+	}
+
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	
+	
+	
+	
+	
+	
+
+}
