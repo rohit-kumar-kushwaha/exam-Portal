019a5f30b3d1c7a9871b53d79230f0d48bfb446b
Rohit kumar <78663043+rohit-kumar-kushwaha@users.noreply.github.com>
2022-04-03 15:56:29 +0530

    add question by frontend

--- frontend/src/app/pages/admin/add-question/add-question.component.ts
+++ frontend/src/app/pages/admin/add-question/add-question.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { QuestionService } from 'src/app/services/question.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-question',
@@ -34,6 +35,47 @@ export class AddQuestionComponent implements OnInit {
     this.qTitle = this._route.snapshot.paramMap.get("title");
     //alert(this.qId);
     this.question.quiz['qid'] = this.qId;
+  }
+
+  questionAddFormSubmit() {
+    //alert("Question added");
+    if(this.question.content.trim()=='' || this.question.content.trim()==null){
+      return;
+    }
+
+    if(this.question.option1.trim()=='' || this.question.option1.trim()==null){
+      return;
+    }
+    if(this.question.option2.trim()=='' || this.question.option2.trim()==null){
+      return;
+    }
+    if(this.question.option3.trim()=='' || this.question.option3.trim()==null){
+      return;
+    }
+    if(this.question.answer.trim()=='' || this.question.answer.trim()==null){
+      return;
+    }
+
+    // form submit
+    this._question.addQuestion(this.question).subscribe(
+      (data:any)=>{
+        Swal.fire("Success","Question Added. Add another one","success");
+        this.question = {
+          quiz: {
+            qid: ''
+          },
+          content: '',
+          option1: '',
+          option2: '',
+          option3: '',
+          option4: '',
+          answer: ''
+        };
+      },
+      (error)=>{
+        Swal.fire("Error !!","Error in adding question","error");
+      }
+    )
   }
 
 }
--- frontend/src/app/services/question.service.ts
+++ frontend/src/app/services/question.service.ts
@@ -14,4 +14,8 @@ export class QuestionService {
   public getQuestionsOfQuiz(qid:any) {
     return this._http.get(`${baseUrl}/question/quiz/all/${qid}`);
   }
+
+  public addQuestion(question:any) {
+    return this._http.post(`${baseUrl}/question/`, question);
+  }
 }
